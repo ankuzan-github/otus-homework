a6ea33b9a0773880bd48d546e0b4a3131ab429b9
Andrey Kuzmin <a.kuzmin@space307.com>
2019-10-11 12:49:13 +0300

    Add task 01 - maven

---
+++ hw01-maven/src/main/java/ru/otus/hw01/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.otus.hw01;
+
+import static com.google.common.collect.Maps.newHashMap;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        var map = newHashMap();
+        System.out.println(map);
+    }
+}
